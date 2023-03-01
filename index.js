9b7d8ac0ca74261385a1d3862292109e49da3056
shafayetullah7 <shafayet769@gmail.com>
2023-03-01 18:05:57 +0600

    I'm lazy

---
+++ index.js
@@ -0,0 +1,83 @@
+let users;
+let sortByRank;
+async function getUsers(){
+    console.log('Hello World');
+    showLoader(document.getElementById('result'));
+    const res = await fetch('https://forbes400.onrender.com/api/forbes400/');
+    const data = await res.json();
+
+    users = [...data];
+    showData(data);
+}
+function showData(data){
+    // console.log(data);
+    // console.log('end hello World');
+    const table = document.getElementById('content-table');
+    let i=0;
+    data.forEach((item)=>{
+        
+        // console.log(item);
+        const tr = document.createElement('tr');
+
+        const name = item?.person?.name;
+        const nametd = document.createElement('td');
+        nametd.innerHTML = name+` <i class="fa-solid fa-eye"></i>`;
+        const citizen = item?.countryOfCitizenship;
+        const citizentd = document.createElement('td');
+        citizentd.innerText = citizen;
+        let industry = item.industries.toString();
+        const industrytd  = document.createElement('td');
+        industrytd.innerText = industry;
+        const rank = item?.rank;
+        const ranktd = document.createElement('td');
+        ranktd.innerText = rank;
+        const wealthtd = document.createElement('td');
+        wealthtd.innerText = 'Unknown';
+
+        tr.append(nametd);
+        tr.append(citizentd);
+        tr.append(industrytd);
+        tr.append(ranktd);
+        tr.append(wealthtd);
+
+        nametd.classList.add('py-2');
+        citizentd.classList.add('py-2');
+        industrytd.classList.add('py-2');
+        ranktd.classList.add('py-2');
+        wealthtd.classList.add('py-2');
+        if(i===0){
+            nametd.classList.add('pt-10');
+            citizentd.classList.add('pt-10');
+            industrytd.classList.add('pt-10');
+            ranktd.classList.add('pt-10');
+            wealthtd.classList.add('pt-10');
+        }
+        i++;
+
+        table.append(tr);
+    });
+    const searchBtn = document.getElementById('search-btn');
+    searchBtn.removeAttribute('disabled');
+    searchBtn.classList.remove('opacity-50');
+
+    hideLoader(document.getElementById('result'));
+}
+document.getElementById('sort-by-rank').addEventListener('click',()=>{
+    getUsers();
+    console.log('HERE');
+});
+
+function showLoader(container){
+    const loader = document.createElement('img');
+    loader.setAttribute('src','./images/icons/loader.svg');
+    loader.id = 'loader'
+    loader.classList.add('block','mx-auto');
+    container.append(loader);
+}
+function hideLoader(container){
+    const loader = document.getElementById('loader');
+    if(loader){
+        container.removeChild(loader);
+    }
+}
+
